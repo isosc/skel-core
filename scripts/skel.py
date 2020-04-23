058e3ae7a970cc9102e0d7bf1cc7e596eb5031e2
Jeremy Logan <jlogan7@utk.edu>
2020-04-23 19:37:05 -0400

    Added skel subcommand for creating an arbitrary usable instance of a model schema

--- scripts/skel.py
+++ scripts/skel.py
@@ -9,7 +9,34 @@ import json
 #import sys
 
 
-def generate(config):
+
+#used by make_instance
+def expand (field_type, schema):
+#    print ("Expand@@@")
+#    print (field_type)
+    if field_type == "int":
+        return 123
+    elif field_type == "string":
+        return "abc"
+    # Handle other primitives here
+    else: # Assume this type is in the schema definition
+        type = schema['types'][field_type] # Lookup type in the schema
+        #probably need some error checking for that last one
+        if type['type'] == "dict":
+            rv = {}
+            for field in type['fields']:
+                rv[field['name']] = expand (field['type'], schema)
+            return rv
+        elif type['type'] == "list":
+            rv = []
+            for i in range(3):
+                rv.append(expand(type['element_type'], schema))
+            return rv
+        else:
+            print ("Unknown object type {}".format(type['type']) )
+            quit()
+
+def process(config):
 
   models = {}
   instantiated_templates = {}
@@ -31,6 +58,18 @@ def generate(config):
 
     with open (config.outfile, 'w') as outfile:
       outfile.write(instantiated_templates[config.outfile])
+    return
+
+  if config.subcommand =="make_instance":
+      out_dict = {}
+      with open (config.schemafile, 'r') as schema_in:
+          schema = json.load(schema_in)
+#          print (schema)
+          for field in schema["fields"]:
+              out_dict[field['name']] = expand (field['type'], schema)
+#          print (out_dict)
+          with open (config.outfile, 'w') as instance_out:
+              json.dump(out_dict, instance_out, indent=4)
 
 import argparse
 
@@ -38,9 +77,13 @@ def commandline (argv):
   parser = argparse.ArgumentParser(prog='skel')
   subparsers = parser.add_subparsers(help='sub help', dest="subcommand")
 
-  _parser_template = subparsers.add_parser('template', help="todo: add subparser help")
+  _parser_template = subparsers.add_parser('template', help="Instantiate a template using one or more named models")
   _parser_template.add_argument('template')
   _parser_template.add_argument('--model', dest="modelfile", required="true")
+  _parser_template.add_argument('--outfile', dest="outfile", required="true")
+
+  _parser_template = subparsers.add_parser('make_instance', help="Create an instance of a given schema suitable for testing")
+  _parser_template.add_argument('--schema', dest="schemafile", required="true")
   _parser_template.add_argument('--outfile', dest="outfile", required="true")
 
   return (parser.parse_args(argv[1:])) # Skip the program name, and pass the rest to the parser
@@ -48,6 +91,6 @@ def commandline (argv):
 def main(argv):
     config = commandline(argv)
     print (config)
-    generate(config)
+    process(config)
 
 
